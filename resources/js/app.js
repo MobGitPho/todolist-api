9803d1c2cde27275857dfe26d162e9e8d88cea4f
MobGitPho <mobphone1212@gmail.com>
2025-08-09 15:16:59 +0000

    creation du store authentification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { createApp } from 'vue'
+import App from './views/App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
---
+++ resources/js/stores/auth.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null,
+    token: localStorage.getItem('token') || null
+  }),
+  actions: {
+    async register(credentials) {
+      const response = await axios.post('/api/auth/register', credentials)
+      localStorage.setItem('token', response.data.token)
+      this.token = response.data.token
+      await this.fetchUser()
+    },
+    async login(credentials) {
+      const response = await axios.post('/api/auth/login', credentials)
+      localStorage.setItem('token', response.data.token)
+      this.token = response.data.token
+      await this.fetchUser()
+    },
+    async fetchUser() {
+      const response = await axios.get('/api/user')
+      this.user = response.data
+    },
+    logout() {
+      localStorage.removeItem('token')
+      this.token = null
+      this.user = null
+    }
+  }
+})
